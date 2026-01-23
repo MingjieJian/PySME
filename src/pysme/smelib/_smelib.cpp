a0d32aea4ed6c34c738ce89c9a607819214b409b
Mingjie Jian <jian-mingjie@outlook.com>
2026-01-24 00:45:25 +0900

    (sme_synth_faster) fix ACCOL opacity extraction bug

--- src/pysme/smelib/_smelib.cpp
+++ src/pysme/smelib/_smelib.cpp
@@ -791,7 +791,8 @@ static PyObject *smelib_GetOpacity(PyObject *self, PyObject *args, PyObject *kwd
     PyArrayObject *arr;
 
     static const char *keywords[] = {"flag", "species", "key", NULL};
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|ss", const_cast<char **>(keywords), &choice))
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|ss", const_cast<char **>(keywords),
+                                     &choice, &species, &key))
         return NULL;
 
     if (strcmp(choice, "COPSTD") == 0)
